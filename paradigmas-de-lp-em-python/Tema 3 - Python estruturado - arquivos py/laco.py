08b5ac186fef8c99b9daafe01443f0ef27c25914
jonlucaz <jonathanlucastri@gmail.com>
2022-09-30 09:58:14 -0300

    Novos arquivos de aulas

---
+++ paradigmas-de-lp-em-python/Tema 3 - Python estruturado - arquivos py/laco.py
@@ -0,0 +1,11 @@
+for item in range(9):
+  print(item)
+
+nome = input("Entre com seu nome: ")
+for letra in nome:
+  print(letra)
+
+nomes = ['Jan', 'Jem', 'Jon', 'Jun']
+for nome in nomes:
+  print(nome)
+  
---
+++ paradigmas-de-lp-em-python/Tema 3 - Python estruturado - arquivos py/teste.py
@@ -0,0 +1,8 @@
+def func1(x): 
+  x = 10
+  print(x)
+
+x = 0
+print(x)
+func1(x)
+print(x)
